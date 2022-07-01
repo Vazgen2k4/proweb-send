a1384edcf55c8ded5ddc7e084d433b6376a7938d
Vazgen <Vazgen2k4@gmail.com>
2022-07-01 16:59:11 +0500

    Добавление цветовой палитры и подключение Spider

--- lib/ui/theme/app_colors.dart
+++ lib/ui/theme/app_colors.dart
@@ -1,3 +1,36 @@
+import 'package:flutter/cupertino.dart';
+
 abstract class AppColors {
   AppColors._();
+
+  static const Color akcent = Color(0xff64d2ff);
+  static const Color akcentLight = Color(0xff5ac8fa);
+
+  static const Color akcentSecondary = Color(0xff64d2ff);
+  static const Color akcentSecondaryLight = Color(0xff0a84ff);
+
+  static const Color bg = Color(0xff000000);
+  static const Color bgLight = Color(0xfff0f0f0);
+
+  static const Color elementsPrimary = Color(0xff1c1c1c);
+  static const Color elementsPrimaryLight = Color(0xff2c2c2c);
+  
+  static const Color elementsSecondary = Color(0xff7c7c80);
+  static const Color elementsSecondaryLight = Color(0xff9b9b9b);
+
+  static const Color text = Color(0xffffffff);
+  static const Color textLight = Color(0xff333333);
+
+  static const Color textInput = Color(0xff646464);
+  static const Color textInputLight = Color(0xff5e5e5e);
+
+  static const Color textInfo = Color(0xff8e8e93);
+  static const Color textInfoLight = Color(0xff000000);
+
+  static const Color textInfoSecondary = Color(0xffb2b2b2);
+  static const Color textInfoSecondaryLight = Color(0xff5e5e5e);
+
+  static const Color error = Color(0xffff3b30);
+  static const Color errorLight = Color(0xffff3b30);
+
 }
