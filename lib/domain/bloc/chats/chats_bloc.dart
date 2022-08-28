f689b85714f780b7dc1a7c3e1749482fe0b8fd9a
Vazgen <Vazgen2k4@gmail.com>
2022-08-28 21:01:19 +0500

    fd

--- lib/domain/bloc/chats/chats_bloc.dart
+++ lib/domain/bloc/chats/chats_bloc.dart
@@ -4,7 +4,6 @@ import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:proweb_send/domain/firebase/firebase_collections.dart';
 import 'package:proweb_send/domain/models/chat_model.dart';
 import 'package:proweb_send/domain/models/pro_user.dart';
